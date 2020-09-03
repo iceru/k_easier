f709ffa35048bfc0515e0f616c2b3e79518a4c94
Reza Mukhlis Qurbany <reza_muqu@roccketmail.com>
2020-09-03 21:32:24 +0700

    add homepage base

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:k_easier/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'K-Easier',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,13 +21,14 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.green,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'K-Easier by CodeOmnia'),
+      // home: MyHomePage(title: 'K-Easier by CodeOmnia'),
+      home: HomePage(),
     );
   }
 }
