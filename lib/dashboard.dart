30e64ca5d893b8a50cba4cb4ebce444deeda228b
hafiz <m.hafiz1825@gmail.com>
2020-09-05 15:54:26 +0700

    dashboard + sign in + transaction

---
+++ lib/dashboard.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:k_easier/transactions.dart';
+
+const mainColor = Color(0xff56C596);
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Dashboard',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        ),
+        backgroundColor: mainColor,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Container(
+          color: mainColor,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Material(
+                    color: mainColor,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Transactions(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: cardDash(Icons.receipt, 'Transaction'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    child: cardDash(Icons.supervisor_account, 'Customers'),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    child: cardDash(Icons.shopping_basket, 'Items'),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    child: cardDash(Icons.show_chart, 'Reports'),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget cardDash(icon, textCard) => Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      margin: EdgeInsets.all(10),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Icon(icon, color: mainColor, size: 40),
+            ),
+            Text(
+              textCard,
+              style: TextStyle(
+                  color: mainColor, fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
---
+++ lib/signIn.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+const mainColor = Color(0xff56C596);
+
+class SignIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomPadding: false,
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Padding(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: titleSection,
+        ),
+      ),
+    );
+  }
+}
+
+Widget titleSection = Container(
+  padding: const EdgeInsets.only(top: 20),
+  color: mainColor,
+  alignment: Alignment(0.0, 0.0),
+  child: Column(
+    children: [
+      Container(
+        padding: const EdgeInsets.all(30),
+        child: (Text(
+          'K-Easier',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.normal,
+            fontSize: 32,
+          ),
+        )),
+      ),
+      Image.asset(
+        'assets/store.png',
+        width: 220,
+      ),
+      Container(
+          margin: const EdgeInsets.only(top: 40),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.white,
+          ),
+          child: signIn),
+      register,
+    ],
+  ),
+);
+
+Widget signIn = Container(
+  padding: const EdgeInsets.all(30),
+  child: Column(
+    textDirection: TextDirection.ltr,
+    children: [
+      Padding(
+        padding: EdgeInsets.only(bottom: 20),
+        child: Text(
+          'Sign In to your Account',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+            color: mainColor,
+          ),
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.symmetric(vertical: 15),
+        child: new Theme(
+          data: new ThemeData(
+            primaryColor: Color(0xff56C596),
+            primaryColorDark: Color(0xff56C596),
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              border: new OutlineInputBorder(
+                borderRadius: const BorderRadius.all(
+                  const Radius.circular(20.0),
+                ),
+                borderSide: const BorderSide(color: mainColor),
+              ),
+              labelText: 'Your Email',
+              labelStyle: new TextStyle(color: mainColor),
+            ),
+          ),
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.symmetric(vertical: 15),
+        child: TextField(
+          decoration: InputDecoration(
+              border: new OutlineInputBorder(
+                borderRadius: const BorderRadius.all(
+                  const Radius.circular(20.0),
+                ),
+                borderSide: const BorderSide(color: mainColor, width: 2),
+              ),
+              labelText: 'Your Password',
+              labelStyle: new TextStyle(color: mainColor)),
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.only(top: 5),
+        child: ButtonTheme(
+          minWidth: double.infinity,
+          height: 50,
+          child: RaisedButton(
+            onPressed: () {},
+            color: mainColor,
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Text(
+              'Sign In',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
+      )
+    ],
+  ),
+);
+
+Widget register = Center(
+  child: Container(
+      alignment: Alignment(0, 0),
+      color: Color(0xffB6E9D3),
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      margin: const EdgeInsets.only(top: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Dont have an Account?',
+            style: TextStyle(fontSize: 16),
+          ),
+          Text(
+            'Register',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          )
+        ],
+      )),
+);
